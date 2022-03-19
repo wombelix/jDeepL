6bf8da2d85f79ef866435ea1b62f1e51bc1ff3f6
Dominik Wombacher <dominik@wombacher.cc>
2022-03-19 17:39:21 +0100

    fix: Use Height / Width as defined in FXML

--- src/main/java/cc/wombacher/dominik/jdeepl/Application.java
+++ src/main/java/cc/wombacher/dominik/jdeepl/Application.java
@@ -10,7 +10,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("jDeepL - Cross Platform GUI");
         stage.setScene(scene);
         stage.show();
