10da77c1052dc7bbfa7dc483db22075e3cee4a38
Dominik Wombacher <dominik@wombacher.cc>
2022-05-29 15:54:07 +0200

    feat(release.sh): Add spdx license header to CHANGELOG.json after generation, push to origin and mirror

--- release.sh
+++ release.sh
@@ -10,9 +10,20 @@ export RELVER=v`./mvnw semantic-gitlog:derive | grep "NEXT_VERSION:==" | grep -o
 # Generate Changelogs and bump version in pom.xml
 ./mvnw semantic-gitlog:changelog
 
+# Ensure spdx / reuse compliant license information are added to generated CHANGELOG.json
+reuse addheader --license CC0-1.0 --copyright-style spdx --year 2022 --copyright "Dominik Wombacher <dominik@wombacher.cc>" CHANGELOG.json
+
 # Add and commit Changelog and pom file
-git add CHANGELOG.md CHANGELOG.json pom.xml
+git add CHANGELOG.md CHANGELOG.json CHANGELOG.json.license pom.xml
 git commit -m "docs: pom.xml version bumped, Changelogs generated for Release $RELVER"
 
 # Tag Release
 git tag $RELVER
+
+# Push Release Origin
+git push origin main
+git push origin $RELVER
+
+# Push Release to Mirrors
+git push mirror main
+git push mirror $RELVER
