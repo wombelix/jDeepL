793dc739b75559204a7cfb63f1fc82e590aa61e9
Dominik Wombacher <dominik@wombacher.cc>
2023-10-08 19:53:17 +0200

    fix: unused reuse commands removed from release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: 2022 Dominik Wombacher <dominik@wombacher.cc>
+# SPDX-FileCopyrightText: 2023 Dominik Wombacher <dominik@wombacher.cc>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -10,11 +10,8 @@ export RELVER=v`./mvnw semantic-gitlog:derive | grep "NEXT_VERSION:==" | grep -o
 # Generate Changelogs and bump version in pom.xml
 ./mvnw semantic-gitlog:changelog
 
-# Ensure spdx / reuse compliant license information are added to generated CHANGELOG.json
-reuse addheader --license CC0-1.0 --copyright-style spdx --year 2022 --copyright "Dominik Wombacher <dominik@wombacher.cc>" CHANGELOG.json
-
 # Add and commit Changelog and pom file
-git add CHANGELOG.md CHANGELOG.json CHANGELOG.json.license pom.xml
+git add CHANGELOG.md CHANGELOG.json pom.xml
 git commit -m "docs: pom.xml version bumped, Changelogs generated for Release $RELVER"
 
 # Tag Release
